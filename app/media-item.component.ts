d38f62f822ece82d2a870e3e788a08d555edf8ed
unknown <pavlovskyidmitriy@gmail.com>
2018-02-28 18:46:08 +0200

    Input added and data binded

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  // В input можно передать параметр строкой, с указанием к чему привязывать контектс
+  @Input() mediaItem;
+
+  handleDelete() {
+    console.log('---Deleting media item')
+  }
+}
