2751612286867358577907ac5885356e4d0ed074
unknown <pavlovskyidmitriy@gmail.com>
2018-02-28 14:50:17 +0200

    main component added

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  //Импорт необходимых модулей для этого модуля
+  imports: [
+    BrowserModule
+  ],
+  // Позволяет использовать компоненты, директивы и пайпы в этом модуле
+  declarations: [
+    AppComponent
+  ],
+  // Помогает ангулару понять какой компонент или компоненты являются корневыми, чтоб использовать бутстрап
+  bootstrap: [
+  AppComponent
+  ]
+})
+export class AppModule {}
