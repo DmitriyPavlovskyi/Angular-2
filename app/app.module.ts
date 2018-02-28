c647b88519f4e118c3f826f096e2e2bd1b5bbb05
unknown <pavlovskyidmitriy@gmail.com>
2018-02-28 16:56:57 +0200

    media item and styles added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
   //Импорт необходимых модулей для этого модуля
@@ -9,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   // Позволяет использовать компоненты, директивы и пайпы в этом модуле
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   // Помогает ангулару понять какой компонент или компоненты являются корневыми, чтоб использовать бутстрап
   bootstrap: [
