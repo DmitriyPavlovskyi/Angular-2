eeebd52d906a685aa37b2c0b4a231e893896f95d
unknown <pavlovskyidmitriy@gmail.com>
2018-02-28 21:25:51 +0200

    Favorite media added

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   //Импорт необходимых модулей для этого модуля
@@ -14,7 +15,8 @@ import { MediaItemListComponent } from './media-item-list.component';
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
+    MediaItemListComponent,
+    FavoriteDirective
   ],
   // Помогает ангулару понять какой компонент или компоненты являются корневыми, чтоб использовать бутстрап
   bootstrap: [
