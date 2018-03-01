ecfe958aa2701b829ad1bf43c10a9a35db5ab5a1
Dmytro_Pavlovskyi1 <dmytro_pavlovskyi1@epam.com>
2018-03-01 12:56:35 +0200

    Simple form module added

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { MediaItemFormComponent } from './media-item-form.component';
 import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
-  //Импорт необходимых модулей для этого модуля
+  //Импорт необходимых модулей
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   // Позволяет использовать компоненты, директивы и пайпы в этом модуле
   declarations: [
@@ -18,6 +21,7 @@ import { CategoryListPipe } from './category-list.pipe';
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
+    MediaItemFormComponent,
     CategoryListPipe
   ],
   // Помогает ангулару понять какой компонент или компоненты являются корневыми, чтоб использовать бутстрап
