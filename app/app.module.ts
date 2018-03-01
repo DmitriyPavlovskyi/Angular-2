b7f6c80a046d6060504e19c4671fa33766e12704
Dmytro_Pavlovskyi1 <dmytro_pavlovskyi1@epam.com>
2018-03-01 13:12:58 +0200

    Form refactored to model driven

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -13,7 +13,7 @@ import { CategoryListPipe } from './category-list.pipe';
   //Импорт необходимых модулей
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   // Позволяет использовать компоненты, директивы и пайпы в этом модуле
   declarations: [
